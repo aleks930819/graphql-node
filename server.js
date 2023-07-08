25c6c26bfa8e0001899d55da848df158f908a293
Aleksandar Atanasov <aleks930819@gmail.com>
2023-07-08 12:39:45 +0300

    addded schema

--- server.js
+++ server.js
@@ -1,12 +1,87 @@
 const express = require('express');
-const {buildSchema}  = require('graphql');
-
+const { buildSchema } = require('graphql');
+const { graphqlHTTP } = require('express-graphql');
 
 const schema = buildSchema(`
-  type 
+  type  Query {
+  products: [Product]
+  orders:[Order]
+  }
+
+  type Review {
+    rating: Int!
+    comment: String
+    }
+
+  type Product {
+    id: ID!
+    name: String!
+    description: String!
+    price: Float!
+    reviews: [Review]
+    }
+
+    type Order {
+        date: String!
+        subtotal: Float!
+        items: [OrderItem]
+    }
+
+    type OrderItem {
+        product: Product!
+        quantity: Int!
+    }
 `);
 
+const root = {
+  products: [
+    {
+      id: '1',
+      description: 'MacBook Pro',
+      price: 5399.99,
+    },
+    {
+      id: '2',
+      description: 'iPhone 11 Pro',
+      price: 5999.99,
+    },
+  ],
+  orders: [
+    {
+      date: '2020-01-01',
+      subtotal: 11399.98,
+      items: [
+        {
+          product: {
+            id: '1',
+            description: 'MacBook Pro',
+            price: 5399.99,
+          },
+          quantity: 1,
+        },
+        {
+          product: {
+            id: '2',
+            description: 'iPhone 11 Pro',
+            price: 5999.99,
+          },
+          quantity: 1,
+        },
+      ],
+    },
+  ],
+};
+
 const app = express();
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
